7bfb5fe464735678412558fc809617fb1b7b3cbf
Robbinson <omoshx78@gmail.com>
2018-02-03 18:49:55 +0300

    add about page

--- server.js
+++ server.js
@@ -12,5 +12,9 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
+app.get('/about', (req,res)=>{
+    res.send("Website coded by robbinson");
+});
+
 
 app.listen(port, () => console.log('Server running'));
